c3029081a27c77db40249b7906b4b70a2f787e96
Alejandro Valdevila Pérez <avaldevilap@gmail.com>
2023-10-08 09:36:41 +0200

    Update index.ts

--- src/resources/organizations/index.ts
+++ src/resources/organizations/index.ts
@@ -1,3 +1,49 @@
-import { Base } from '..';
+import invariant from 'tiny-invariant';
+import { z } from 'zod';
 
-export class OrganizationApi extends Base {}
+import { Base } from '..';
+import { AccountSchema, OrganizationSchema } from './types';
+
+export class OrganizationApi extends Base {
+  get() {
+    return this.request(OrganizationSchema, '/organization');
+  }
+
+  create(name: string) {
+    invariant(name, 'Name is required');
+
+    const body = JSON.stringify({ name });
+
+    return this.request(OrganizationSchema, '/organization', {
+      method: 'POST',
+      body,
+    });
+  }
+
+  rename(name: string) {
+    invariant(name, 'Name is required');
+
+    const body = JSON.stringify({ name });
+
+    return this.request(OrganizationSchema, '/organization', {
+      method: 'PUT',
+      body,
+    });
+  }
+
+  addAccount(organization_id: string, organization_token: string) {
+    invariant(organization_id, 'Organization id is required');
+    invariant(organization_token, 'Organization token is required');
+
+    const body = JSON.stringify({ organization_id, organization_token });
+
+    return this.request(z.array(AccountSchema), '/organization/accounts', {
+      method: 'POST',
+      body,
+    });
+  }
+
+  listAccounts() {
+    return this.request(z.array(AccountSchema), '/organization/accounts');
+  }
+}
